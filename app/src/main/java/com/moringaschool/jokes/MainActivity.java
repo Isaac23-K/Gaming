01f061614cdb0616f3281c52defc308491df4c2d
Isaac23-K <ikaptoge@yahoo.com>
2021-09-10 14:34:44 +0300

    created new class adapter

--- app/src/main/java/com/moringaschool/jokes/MainActivity.java
+++ app/src/main/java/com/moringaschool/jokes/MainActivity.java
@@ -2,12 +2,15 @@ package com.moringaschool.jokes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
     @BindView(R.id.findJokesButton)
@@ -22,5 +25,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mFindJokesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String type = mEditText.getText().toString();
+                Intent intent = new Intent(MainActivity.this,jokesList.class);
+                intent.putExtra("jokes", type);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/moringaschool/jokes/MyJokesArrayAdapter.java
@@ -0,0 +1,8 @@
+package com.moringaschool.jokes;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+public class MyJokesArrayAdapter  extends ArrayAdapter {
+    private Context mContext;
+}
