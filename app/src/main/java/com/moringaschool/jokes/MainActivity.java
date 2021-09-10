cf27bfc8f8af3f1222622c212493aaabe0bd24af
Isaac23-K <ikaptoge@yahoo.com>
2021-09-10 13:38:44 +0300

    added code for the main activity

--- app/src/main/java/com/moringaschool/jokes/MainActivity.java
+++ app/src/main/java/com/moringaschool/jokes/MainActivity.java
@@ -3,8 +3,20 @@ package com.moringaschool.jokes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.findJokesButton)
+    Button mFindJokesButton ;
+    @BindView(R.id.appNameTextView)
+    TextView mAppNameTextView ;
+    @BindView(R.id.typeEditText)
+    EditText mEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
