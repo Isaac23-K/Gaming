a226cd0246f8b7a724684c73c8f55ff629e4c777
Isaac23-K <ikaptoge@yahoo.com>
2021-09-22 09:53:04 +0300

    added new code

--- app/src/main/java/com/moringaschool/jokes/Adapters/CategoryAdapter.java
+++ app/src/main/java/com/moringaschool/jokes/Adapters/CategoryAdapter.java
@@ -1,11 +1,13 @@
 package com.moringaschool.jokes.Adapters;
 
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.moringaschool.jokes.R;
@@ -14,6 +16,7 @@ import java.util.List;
 
 public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHolder> {
     List<String> cats;
+    int positionOfCard;
 
     public CategoryAdapter(List<String> cats){
         this.cats = cats;
@@ -22,13 +25,20 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHo
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull  ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.category_view,false);
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.category_view,parent,false);
         return new ViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(@NonNull CategoryAdapter.ViewHolder holder, int position) {
     holder.catName.setText(cats.get(position));
+        if(positionOfCard == position){
+        //set color bg
+holder.cardItem.setCardBackgroundColor(Color.RED);
+
+    } else {
+holder.cardItem.setCardBackgroundColor(Color.WHITE);
+    }
     }
 
     @Override
@@ -36,11 +46,21 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHo
         return cats.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         TextView catName;
+        CardView cardItem ;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             catName = itemView.findViewById(R.id.catName);
+            cardItem = itemView.findViewById(R.id.cardItem);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            notifyItemChanged(positionOfCard);
+            positionOfCard = getAdapterPosition();
+            notifyItemChanged(positionOfCard);
         }
     }
 }
