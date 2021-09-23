f62577b45b81cd5c5a6f80e6c044fa096736ee7b
Isaac23-K <ikaptoge@yahoo.com>
2021-09-23 10:27:22 +0300

    added dependencies

---
+++ app/src/main/java/com/moringaschool/jokes/Constants.java
@@ -0,0 +1,7 @@
+package com.moringaschool.jokes;
+
+public class Constants {
+    public static final String Joke = "https://v2.jokeapi.dev/joke/";
+    public static final String JokeAPi = BuildConfig.APPLICATION_ID;
+
+}
---
+++ app/src/main/java/com/moringaschool/jokes/network/JokeClient.java
@@ -0,0 +1,5 @@
+package com.moringaschool.jokes.network;
+
+public class JokeClient {
+
+}
