385357c8175f4c8ab5ff7182d2d3a7de6998bc86
Isaac23-K <ikaptoge@yahoo.com>
2021-09-29 08:25:25 +0300

    added new animation

--- app/src/main/java/com/moringaschool/jokes/ui/MainActivity.java
+++ app/src/main/java/com/moringaschool/jokes/ui/MainActivity.java
@@ -9,6 +9,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -30,11 +32,23 @@ public class MainActivity extends AppCompatActivity {
 //    @BindView(R.id.appNameTextView)
 //    TextView mAppNameTextView ;
 
+    Button b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b = (Button)findViewById(R.id.btn);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Animation animation = AnimationUtils.loadAnimation(MainActivity.this,R.anim.zoomout);
+                b.startAnimation(animation);
+            }
+        });
+
 
         categories = new ArrayList<>();
         // cat names
